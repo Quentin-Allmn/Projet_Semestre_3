1e39cecd96363f7c4ba23d96f1de4f024814bc11
Quentin-Allmn <q.alleman@mtp.ecolescreatives.com>
2022-11-27 15:07:47 +0100

    ça se met en place

--- Projet_Unity/Assets/Scripts/GameManager.cs
+++ Projet_Unity/Assets/Scripts/GameManager.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    public bool mission3IsSucess = false;
+
+    public int kingHealth = 100;
+
+    LoadLevel LoadLevel;
+
+    private void Awake()
+    {
+        LoadLevel = FindObjectOfType<LoadLevel>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (kingHealth <= 0)
+        {
+            Debug.Log("Game Over");
+            LoadLevel.LevelLoader(5);
+        }
+
+        if (mission3IsSucess == true)
+        {
+            Debug.Log("Victory");
+            LoadLevel.LevelLoader(5);
+        }
+
     }
 }
