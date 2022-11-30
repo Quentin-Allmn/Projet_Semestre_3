0e12f81445ef79fe967d1eca095af47dde89be70
Quentin-Allmn <q.alleman@mtp.ecolescreatives.com>
2022-11-30 11:24:20 +0100

    MinniGame2

--- Projet_Unity/Assets/Scripts/Saignee/SaigneeManager.cs
+++ Projet_Unity/Assets/Scripts/Saignee/SaigneeManager.cs
@@ -8,7 +8,9 @@ public class SaigneeManager : MonoBehaviour
 
     [SerializeField] public List<Bleeding> listBleeding = new List<Bleeding>();
     [SerializeField] GameObject stockOintment;
+
     [SerializeField] Image victory;
+    [SerializeField] Image defeat;
 
     public int bleedingCount = 0;
     public int ointmentCount = 0;
@@ -16,10 +18,19 @@ public class SaigneeManager : MonoBehaviour
     public bool bleedingFinished = false;
     public bool ointmentFinished = false;
 
+    private float timeMax = 25f;
+    //private float timeMin = 0f;
+    private float timeOintment;
+    private float timeBleeding;
+
     private void Start()
     {
-        
+        timeOintment = timeMax / 2;
+        timeBleeding = timeMax;
+
     }
+
+
 
     private void Update()
     {
@@ -27,7 +38,6 @@ public class SaigneeManager : MonoBehaviour
         if (bleedingCount == listBleeding.Count)
         {
             stockOintment.SetActive(true);
-            bleedingFinished = true;
         }
 
         if (ointmentCount == listBleeding.Count)
@@ -35,6 +45,26 @@ public class SaigneeManager : MonoBehaviour
             ointmentFinished = true;
             victory.gameObject.SetActive(true);
         }
+
+        if (bleedingCount > 0)
+        {
+
+            timeBleeding -= Time.deltaTime;
+            Debug.Log("ca saigne");
+
+        }
+
+
+        if (timeBleeding < timeOintment)
+        {
+            Debug.Log("Faut Heal");
+            bleedingFinished = true;
+        }
+
+        if (timeBleeding <= 0)
+        {
+            defeat.gameObject.SetActive(true);
+        } 
 
     }
 
